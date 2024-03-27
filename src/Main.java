41097071f4131f26f4770cdade5e4e2c18c39615
Artem <artem.yaremenko.1992@gmail.com>
2024-03-27 23:41:11 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         byte a = 8;
         short b = 512;
         int c = 149_310_000; //Расстояние от Земли до Солнца в км
-        long d =  4_550_000_000L;//Расстояние от Солнца до Нептуна в км
+        long d =  4_550_000_000L; //Расстояние от Солнца до Нептуна в км
         float e = 3.14f;
         double f = 3.141592653589;
 
@@ -16,5 +16,14 @@ public class Main {
         System.out.println("Значение переменной e с типом float равно " + e);
         System.out.println("Значение переменной f с типом double равно " + f);
 
+        // Task 2
+        float flo = 27.12f;
+        long lon = 987_678_965_549L;
+        double dou = 2.786;
+        short sho = 569;
+        int in = 27897;
+        byte byt = 67;
+
+        // Task 3
     }
 }
