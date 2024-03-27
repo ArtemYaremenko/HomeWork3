733a967273412660f5c5489d888328f68c5ef7c6
Artem <artem.yaremenko.1992@gmail.com>
2024-03-27 23:29:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Task 1
+        byte a = 8;
+        short b = 512;
+        int c = 149_310_000; //Расстояние от Земли до Солнца в км
+        long d =  4_550_000_000L;//Расстояние от Солнца до Нептуна в км
+        float e = 3.14f;
+        double f = 3.141592653589;
+
+        System.out.println("Значение переменной a с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+
+    }
+}
